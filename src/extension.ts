fe11a17e8e890bc667865455faa49feb2fbf7c90
Bryan Webb <bryan.webb@aventiv.com>
2021-04-05 16:15:20 -0400

    - Tested the change of text.

--- src/extension.ts
+++ src/extension.ts
@@ -14,10 +14,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('CreatePackageFromGit.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		// The code you place here w5ill be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SFDX Git Package Creator!');
+		vscode.window.showInformationMessage('Hello from SFDX Git Package Creator!');
 	});
 
 	context.subscriptions.push(disposable);
