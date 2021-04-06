a46506ad29aa29bf31cb11d9cc8d0c7283131fdd
Bryan Webb <bryan.webb@aventiv.com>
2021-04-06 11:18:58 -0400

    - Added a Class and functions to start dev work.

---
+++ src/MainClass.ts
@@ -0,0 +1,12 @@
+export class MainClass {
+
+    static run() {
+        console.log('In the main function');
+        this.getRepoPath();
+    }
+
+    static getRepoPath(){
+        console.log('getRepoFromPath');
+    }
+
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { MainClass } from './MainClass';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,6 +14,8 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showInformationMessage('Hello from SFDX Git Package Creator!');
 		})
 	);
+
+	MainClass.run();
 }
 
 // this method is called when your extension is deactivated
