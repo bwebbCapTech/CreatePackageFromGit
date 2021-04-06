811b3200b063943960d0aacab4758ccd7b916531
Bryan Webb <bryan.webb@aventiv.com>
2021-04-06 10:45:27 -0400

    - Cleanup.

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,13 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "CreatePackageFromGit" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('CreatePackageFromGit.helloWorld', () => {
-		// The code you place here w5ill be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello from SFDX Git Package Creator!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'CreatePackageFromGit.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello from SFDX Git Package Creator!');
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
